82e846bbb5e3e22ea0297b42732a2f2d2bb83dc6
Giovanni Tamanini <giovanni.tnini@gmail.com>
2024-05-26 10:30:45 -0300

    Updating end of line sequence

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,15 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CreditCardModule } from './credit-card/credit-card.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [CreditCardModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    CreditCardModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
